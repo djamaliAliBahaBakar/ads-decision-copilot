0b4da155074083bdbd23be9ef5095ffc8c8a8831
Djamali Ali bakar <dalibakar@gmail.com>
2026-01-30 21:13:43 +0100

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -19,7 +19,7 @@ export default clerkMiddleware(async (auth, request) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    // Exclude static files and specific API routes from middleware
+    '/((?!_next|api/stripe/webhook|api/cron|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
 }
